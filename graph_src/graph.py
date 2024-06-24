d02065c03bd093b4f6e6615e95aa2cf5d7b50d83
natriistorm <natriistorm@mail.ru>
2024-06-24 14:11:04 +0300

    Changed responses & requests to utterances

--- graph_src/graph.py
+++ graph_src/graph.py
@@ -1,9 +1,11 @@
 from enum import Enum
 import networkx as nx
 
+
 class TYPES_OF_GRAPH(Enum):
-    DI = 1 # if we do not allow multiedges
-    MULTI = 2 # if we allow multiedges
+    DI = 1  # if we do not allow multiedges
+    MULTI = 2  # if we allow multiedges
+
 
 class Graph:
     def __init__(self, graph: dict, graph_type: TYPES_OF_GRAPH) -> None:
@@ -14,11 +16,11 @@ class Graph:
         self.type = graph_type
         self.graph_degrees = []
         for node in graph['nodes']:
-            if type(node['response']) is list:
-                self.nx_graph.add_node(node['id'], responses=node['response'])
+            if type(node['utterances']) is list:
+                self.nx_graph.add_node(node['id'], utterances=node['utterance'])
             else:
-                self.nx_graph.add_node(node['id'], responses=[node['response']])
+                self.nx_graph.add_node(node['id'], utterances=[node['utterance']])
         for link in graph['edges']:
             first = link['source']
             second = link['target']
-            self.nx_graph.add_edges_from([(first, second, {"requests": link['request']})])
+            self.nx_graph.add_edges_from([(first, second, {"utterances": link['utterances']})])
