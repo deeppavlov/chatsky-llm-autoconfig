f1c445b22c3698f854e8554ebef644ef619945c2
natriistorm <natriistorm@mail.ru>
2024-06-25 12:44:18 +0300

    Added node renumeration

--- graph_src/graph.py
+++ graph_src/graph.py
@@ -15,12 +15,30 @@ class Graph:
             self.nx_graph = nx.DiGraph()
         self.type = graph_type
         self.graph_degrees = []
+        nodes  = sorted([v['id'] for v in graph['nodes']])
+        self.node_mapping = {}
+        renumber_flg = False
+        if nodes != [i for i in range(1, len(nodes) + 1)]:
+            renumber_flg = True
+            for j in range(len(nodes)):
+                self.node_mapping[nodes[j]] = j + 1
+
+
         for node in graph['nodes']:
+            cur_node_id = node['id']
+            if renumber_flg:
+                cur_node_id = self.node_mapping[cur_node_id]
+            
             if type(node['utterances']) is list:
-                self.nx_graph.add_node(node['id'], utterances=node['utterances'])
+                self.nx_graph.add_node(cur_node_id, utterances=node['utterances'])
             else:
-                self.nx_graph.add_node(node['id'], utterances=[node['utterances']])
+                self.nx_graph.add_node(cur_node_id, utterances=[node['utterances']])
+    
+        
         for link in graph['edges']:
-            first = link['source']
-            second = link['target']
-            self.nx_graph.add_edges_from([(first, second, {"utterances": link['utterances']})])
+                first = link['source']
+                second = link['target']
+                if renumber_flg:
+                    first = self.node_mapping[first]
+                    second = self.node_mapping[second]
+                self.nx_graph.add_edges_from([(first, second, {"utterances": link['utterances']})])
