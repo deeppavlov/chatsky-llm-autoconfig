107d6ae599d9638470bfd6fd15d6a6c25ea73e0e
natriistorm <natriistorm@mail.ru>
2024-07-03 13:29:32 +0300

    Updated source files

--- graph_src/sample_dialogue.py
+++ graph_src/sample_dialogue.py
@@ -1,20 +1,22 @@
 import random
 import json
-from graph_src.graph import Graph, TYPES_OF_GRAPH
+from graph import Graph, TYPES_OF_GRAPH
 
 
-def sample_dialogue(graph_obj, start_node, end_node=None):
-    # Find the start node
+def sample_dialogue(graph_obj, start_node, end_node=None, topic=None):
     nodes = graph_obj.nodes(data=True)
     edges = graph_obj.edges(data=True)
     current_node_id = start_node
     current_node = nodes[current_node_id]
     dialogue = []
     graph = {"nodes": [], "edges": []}
+    appended_nodes = set()
 
     while not (current_node_id == start_node and dialogue != []) or current_node_id == end_node:
+
         utterance = random.choice(current_node["utterances"])
         dialogue.append({"text": utterance, "participant": "assistant"})
+
 
         graph['nodes'].append({
             "id": current_node_id,
@@ -23,16 +25,20 @@ def sample_dialogue(graph_obj, start_node, end_node=None):
             "utterances": [utterance],
         })
 
-
         possible_edges = [edge for edge in edges if edge[0] == current_node_id]
-
         if not possible_edges:
             break
-        chosen_edge = random.choice(possible_edges)
+
+        if topic is not None:
+            chosen_edge = random.choice(possible_edges)
+            while graph_obj.edges[chosen_edge[0], chosen_edge[1]]['theme'] != topic:
+                chosen_edge = random.choice(possible_edges)
+
         if isinstance(chosen_edge[2]["utterances"], list):
             edge_utterance = random.choice(chosen_edge[2]["utterances"])
         else:
             edge_utterance = chosen_edge[2]["utterances"]
+
         dialogue.append({
             "text": edge_utterance, "participant": "user",
             "source": chosen_edge[0],
@@ -56,7 +62,7 @@ with open(f'{path_prefix}/dataset/theme_graph.json', 'r') as f:
     content = json.load(f)
 graph = Graph(content, TYPES_OF_GRAPH.DI)
 sampled_dialogue, sampled_base_graph = sample_dialogue(graph.nx_graph, start_node=1,
-                                   end_node=9)
+                                   end_node=9, topic='videogames')
 
 with open(f'{path_prefix}/dataset/theme_sampled_graph.json', 'r') as g:
     exisint_graph = json.load(g)
