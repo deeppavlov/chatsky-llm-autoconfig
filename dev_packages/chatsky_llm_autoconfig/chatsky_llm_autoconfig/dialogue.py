c1cf2edce76845f254c6ef6dc9a6a69ee9f32445
askatasuna <andruhabobr@gmail.com>
2024-10-18 13:06:45 +0300

    Working on creating base classes for everything

---
+++ dev_packages/chatsky_llm_autoconfig/chatsky_llm_autoconfig/dialogue.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel
+from typing import Optional, Any, Union
+import json
+import matplotlib.pyplot as plt
+import abc
+import logging
+logger = logging.getLogger(__name__)
+
+
+class Dialogue(BaseModel, abc.ABC):
+    """
+    Base class for all of the dialogues.
+
+    Attributes
+    ----------
+    dialogue : list of dict
+        A list containing dialogue entries, where each entry is a 
+        dictionary with "text" and "participant" keys. 
+        It is initialized with an empty dictionary.
+
+    Examples
+    --------
+        Dialogue([{"text": "How can I help?", "participant": "assistant"}, {"text": "I need to make an order", "participant": "user"}])
+
+    """
+    dialogue : list[dict] = [{}]
+    class Config:
+        arbitrary_types_allowed = True
+
+    def parse_string(self, string):
+        self.dialogue = []
+        for utt in string.split('\n'):
+            participant, phrase = utt.split('\t')
+            self.dialogue.append({
+                "text": phrase,
+                "participant": participant
+            })
+
+    def __str__(self):
+        readable = "\n".join([utt['participant'] + ": " + utt['text'] for utt in self.dialogue])
+        return readable.strip()
--- dev_packages/chatsky_llm_autoconfig/chatsky_llm_autoconfig/graph.py
+++ dev_packages/chatsky_llm_autoconfig/chatsky_llm_autoconfig/graph.py
@@ -1,27 +1,54 @@
 from enum import Enum
 import networkx as nx
-
+from pydantic import BaseModel
+from typing import Optional, Any, Union
+import json
+import matplotlib.pyplot as plt
+import abc
+import logging
+logger = logging.getLogger(__name__)
 
 class TYPES_OF_GRAPH(Enum):
     DI = 1  # if we do not allow multiedges
     MULTI = 2  # if we allow multiedges
 
 
-class Graph:
-    def __init__(self, graph: dict, graph_type: TYPES_OF_GRAPH) -> None:
-        self.nx_graph = nx.MultiDiGraph() if graph_type == TYPES_OF_GRAPH.MULTI else nx.DiGraph()
-        self.type = graph_type
-        print("Graph type:", type(graph))
-        nodes = sorted([v["id"] for v in graph["nodes"]])
-        print(nodes)
+class BaseGraph(BaseModel, abc.ABC):
+    graph_dict: dict
+    graph: Optional[nx.Graph] = None
+    graph_type: Union[1, 2] = 1
+    node_mapping: Optional[dict] = None
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    @abc.abstractmethod
+    def load_graph(self, *args, **kwargs):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def visualise(self, *args, **kwargs):
+        raise NotImplementedError
+
+class Graph(BaseGraph):
+
+    def __init__(self, graph_dict: dict, **kwargs: Any):
+        super().__init__(graph_dict=graph_dict, **kwargs)  # Pass graph_dict to the parent class
+        self.load_graph()
+
+    def load_graph(self):
+        self.graph = nx.MultiDiGraph() if self.graph_type == TYPES_OF_GRAPH.MULTI else nx.DiGraph()
+        nodes = sorted([v["id"] for v in self.graph_dict["nodes"]])
+        logging.debug(f"Nodes: {nodes}")
 
         self.node_mapping = {}
         renumber_flg = nodes != list(range(1, len(nodes) + 1))
         if renumber_flg:
             self.node_mapping = {node_id: idx + 1 for idx, node_id in enumerate(nodes)}
-        print(renumber_flg)
+        logging.debug(f"Renumber flag: {renumber_flg}")
 
-        for node in graph["nodes"]:
+
+        for node in self.graph_dict["nodes"]:
             cur_node_id = node["id"]
             if renumber_flg:
                 cur_node_id = self.node_mapping[cur_node_id]
@@ -29,11 +56,29 @@ class Graph:
             theme = node.get("theme")
             label = node.get("label")
             if type(node["utterances"]) is list:
-                self.nx_graph.add_node(cur_node_id, theme=theme, label=label, utterances=node["utterances"])
+                self.graph.add_node(cur_node_id, theme=theme, label=label, utterances=node["utterances"])
             else:
-                self.nx_graph.add_node(cur_node_id, theme=theme, label=label, utterances=[node["utterances"]])
+                self.graph.add_node(cur_node_id, theme=theme, label=label, utterances=[node["utterances"]])
 
-        for link in graph["edges"]:
+        for link in self.graph_dict["edges"]:
             source = self.node_mapping.get(link["source"], link["source"])
             target = self.node_mapping.get(link["target"], link["target"])
-            self.nx_graph.add_edges_from([(source, target, {"theme": link.get("theme"), "utterances": link["utterances"]})])
+            self.graph.add_edges_from([(source, target, {"theme": link.get("theme"), "utterances": link["utterances"]})])
+        
+    def visualise(self, *args, **kwargs):
+        pos = nx.kamada_kawai_layout(self.graph)
+        nx.draw(self.graph, pos, with_labels=False, node_color="lightblue", node_size=500, font_size=8, arrows=True)
+        edge_labels = nx.get_edge_attributes(self.graph, "label")
+        node_labels = nx.get_node_attributes(self.graph, "label")
+        nx.draw_networkx_edge_labels(self.graph, pos, edge_labels=edge_labels, font_size=12)
+        nx.draw_networkx_labels(self.graph, pos, labels=node_labels, font_size=10)
+
+        plt.title(__name__)
+        plt.axis("off")
+        plt.show()
+
+if __name__=="__main__":
+    with open('/home/askatasuna/Документы/DeepPavlov/chatsky-llm-autoconfig/data/data.json') as f:
+        d = json.load(f)
+    g = Graph(d[0]["target_graph"])
+    # g.visualise()
