0249f2c3a24d2755656111b9d3e1df05caefa516
askatasuna <andruhabobr@gmail.com>
2024-11-06 01:06:45 +0300

    lint

--- dev_packages/chatsky_llm_autoconfig/chatsky_llm_autoconfig/autometrics/registry.py
+++ dev_packages/chatsky_llm_autoconfig/chatsky_llm_autoconfig/autometrics/registry.py
@@ -6,26 +6,23 @@ algorithms for automatic metrics gathering.
 
 """
 
+
 class AlgorithmRegistry:
     _algorithms = {}
-    
+
     @classmethod
     def register(cls, name=None, input_type=None, output_type=None):
         def decorator(func):
             algorithm_name = name or func.__name__
-            cls._algorithms[algorithm_name] = {
-                'type': func,
-                'input_type': input_type,
-                'output_type': output_type
-            }
+            cls._algorithms[algorithm_name] = {"type": func, "input_type": input_type, "output_type": output_type}
             return func
+
         return decorator
-    
+
     @classmethod
     def get_all(cls):
         return cls._algorithms
-    
+
     @classmethod
     def get_by_category(cls, category):
-        return {name: data for name, data in cls._algorithms.items() 
-            if data['category'] == category}
+        return {name: data for name, data in cls._algorithms.items() if data["category"] == category}
