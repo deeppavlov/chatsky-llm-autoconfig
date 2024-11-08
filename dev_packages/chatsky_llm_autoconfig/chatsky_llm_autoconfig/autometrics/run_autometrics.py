57ff13ebf92be5df0f4e03625676fe45b67fb13c
askatasuna <andruhabobr@gmail.com>
2024-11-08 13:00:11 +0300

    Fixed are_triplets_valid metric

--- dev_packages/chatsky_llm_autoconfig/chatsky_llm_autoconfig/autometrics/run_autometrics.py
+++ dev_packages/chatsky_llm_autoconfig/chatsky_llm_autoconfig/autometrics/run_autometrics.py
@@ -7,20 +7,28 @@ import json
 from chatsky_llm_autoconfig.graph import Graph, BaseGraph
 from chatsky_llm_autoconfig.dialogue import Dialogue
 from chatsky_llm_autoconfig.metrics.automatic_metrics import *
+from chatsky_llm_autoconfig.metrics.llm_metrics import are_triplets_valid
 import datetime
 from colorama import Fore
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+load_dotenv() 
 
+
+model = ChatOpenAI(model="gpt-4o", api_key=os.getenv("OPENAI_API_KEY"), base_url=os.getenv("OPENAI_BASE_URL"), temperature=0)
 
 with open("dev_packages/chatsky_llm_autoconfig/chatsky_llm_autoconfig/autometrics/test_data/data.json") as f:
     test_data = json.load(f)
     graph_to_dialogue = test_data["graph_to_dialogue"]
     dialogue_to_dialogue = test_data["dialogue_to_dialogue"]
 
+
 def run_all_algorithms():
     # Get all registered classes
     algorithms = AlgorithmRegistry.get_all()
     print("Classes to test:", *algorithms.keys(), sep="\n")
-    print("------------------\n\n")
+    print("------------------\n")
     total_metrics = {}
     for class_ in algorithms:
         class_instance = algorithms[class_]["type"]()
@@ -28,19 +36,22 @@ def run_all_algorithms():
 
         if algorithms[class_]["input_type"] is BaseGraph and algorithms[class_]["output_type"] is Dialogue:
             metrics = {
-                    "all_paths_sampled": [],
-                    "all_utterances_present": [],
-                }
+                "all_paths_sampled": [],
+                "all_utterances_present": [],
+                "are_triplets_valid": []
+            }
             for case in graph_to_dialogue:
                 test_dialogue = Dialogue(dialogue=case["dialogue"])
                 test_graph = Graph(graph_dict=case["graph"])
                 result = class_instance.invoke(test_graph)
-                
+
                 metrics["all_paths_sampled"].append(all_paths_sampled(test_graph, result[0]))
                 metrics["all_utterances_present"].append(all_utterances_present(test_graph, result))
-            
-            metrics["all_paths_sampled_avg"] = sum(metrics["all_paths_sampled"])/len(metrics["all_paths_sampled"])
-            metrics["all_utterances_present_avg"] = sum(metrics["all_utterances_present"])/len(metrics["all_utterances_present"])
+                metrics["are_triplets_valid"].append(are_triplets_valid(result, model, topic="")["value"])
+
+            metrics["all_paths_sampled_avg"] = sum(metrics["all_paths_sampled"]) / len(metrics["all_paths_sampled"])
+            metrics["all_utterances_present_avg"] = sum(metrics["all_utterances_present"]) / len(metrics["all_utterances_present"])
+            metrics["are_triplets_valid"] = sum(metrics["are_triplets_valid"]) / len(metrics["are_triplets_valid"])
 
         elif algorithms[class_]["input_type"] is Dialogue and algorithms[class_]["output_type"] is Dialogue:
             metrics = {
@@ -53,24 +64,30 @@ def run_all_algorithms():
 
                 metrics["all_roles_correct"].append(all_roles_correct(test_dialogue, result))
                 metrics["is_correct_lenght"].append(is_correct_lenght(test_dialogue, result))
-            
-            metrics["all_roles_correct_avg"] = sum(metrics["all_roles_correct"])/len(metrics["all_roles_correct"])
-            metrics["is_correct_lenght_avg"] = sum(metrics["is_correct_lenght"])/len(metrics["is_correct_lenght"])
-        
+
+            metrics["all_roles_correct_avg"] = sum(metrics["all_roles_correct"]) / len(metrics["all_roles_correct"])
+            metrics["is_correct_lenght_avg"] = sum(metrics["is_correct_lenght"]) / len(metrics["is_correct_lenght"])
+
         elif algorithms[class_]["input_type"] is str and algorithms[class_]["output_type"] is BaseGraph:
             result = class_instance.invoke(test_dialogue)
-            metrics = {
-                
-            }
+            metrics = {}
+        
+        elif algorithms[class_]["input_type"] is BaseGraph and algorithms[class_]["output_type"] is BaseGraph:
+            result = class_instance.invoke(test_dialogue)
+            metrics = {}
 
         total_metrics[class_] = metrics
 
     return total_metrics
 
-def compare_results(date, old_data):
+
+def compare_results(date, old_data, compare_to: str=""):
 
     current_metrics = old_data.get(date, {})
-    previous_date = list(old_data.keys())[-2]
+    if previous_date == "":
+        previous_date = list(old_data.keys())[-2]
+    else:
+        previous_date = compare_to
     previous_metrics = old_data.get(previous_date, {})
 
     differences = {}
@@ -78,22 +95,26 @@ def compare_results(date, old_data):
         if algorithm in previous_metrics:
             differences[algorithm] = {
                 "all_paths_sampled_diff": metrics.get("all_paths_sampled_avg", 0) - previous_metrics[algorithm].get("all_paths_sampled_avg", 0),
-                "all_utterances_present_diff": metrics.get("all_utterances_present_avg", 0) - previous_metrics[algorithm].get("all_utterances_present_avg", 0),
+                "all_utterances_present_diff": metrics.get("all_utterances_present_avg", 0)
+                - previous_metrics[algorithm].get("all_utterances_present_avg", 0),
                 "all_roles_correct_diff": metrics.get("all_roles_correct_avg", 0) - previous_metrics[algorithm].get("all_roles_correct_avg", 0),
                 "is_correct_length_diff": metrics.get("is_correct_lenght_avg", 0) - previous_metrics[algorithm].get("is_correct_lenght_avg", 0),
+                "are_triplets_valid_diff": metrics.get("are_triplets_valid", 0) - previous_metrics[algorithm].get("are_triplets_valid", 0),
                 "total_diff": (
-                    metrics.get("all_paths_sampled_avg", 0) +
-                    metrics.get("all_utterances_present_avg", 0) +
-                    metrics.get("all_roles_correct_avg", 0) +
-                    metrics.get("is_correct_lenght_avg", 0)
-                ) - (
-                    previous_metrics[algorithm].get("all_paths_sampled_avg", 0) +
-                    previous_metrics[algorithm].get("all_utterances_present_avg", 0) +
-                    previous_metrics[algorithm].get("all_roles_correct_avg", 0) +
-                    previous_metrics[algorithm].get("is_correct_lenght_avg", 0)
+                    metrics.get("all_paths_sampled_avg", 0)
+                    + metrics.get("all_utterances_present_avg", 0)
+                    + metrics.get("all_roles_correct_avg", 0)
+                    + metrics.get("is_correct_lenght_avg", 0)
+                    + metrics.get("are_triplets_valid", 0)
                 )
+                - (
+                    previous_metrics[algorithm].get("all_paths_sampled_avg", 0)
+                    + previous_metrics[algorithm].get("all_utterances_present_avg", 0)
+                    + previous_metrics[algorithm].get("all_roles_correct_avg", 0)
+                    + previous_metrics[algorithm].get("is_correct_lenght_avg", 0)
+                    + previous_metrics[algorithm].get("are_triplets_valid", 0)
+                ),
             }
-
     for algorithm, diff in differences.items():
         print(f"Algorithm: {algorithm}")
         for metric, value in diff.items():
